d35da8c109ad9d74552f8988d768241a59bacb59
ryannitz <ryan.nitz01@gmail.com>
2020-02-04 17:43:30 -0400

    Initial game logic completed

---
+++ Assets/Scripts/FlyingCraftController.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FlyingCraftController : MonoBehaviour
+{
+
+    private float rotationAngleDegree;
+    private float rotationSpeedDegree;
+    int rotationDirection;
+
+    private Vector3 motionVelocity;
+    private float motionSpeedXZ;
+    private int motionDirectionXZ;
+
+    private float motionSpeedY;
+    private int motionDirectionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotationAngleDegree = 0;
+        rotationSpeedDegree = 10;
+        rotationDirection = 0;
+
+        motionVelocity = Vector3.zero;
+        motionSpeedXZ = 10;
+        motionDirectionXZ = 0;
+
+        motionSpeedY = 0.5f;
+        motionDirectionY = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        motionDirectionXZ = 1;
+        Move();
+
+        if (Input.GetKey("right"))
+        {
+            rotationDirection = 2;
+            Rotate();
+        }
+
+        if (Input.GetKey("left"))
+        {
+            rotationDirection = -2;
+            Rotate();
+        }
+
+        if (Input.GetKey("up"))
+        {
+            motionDirectionY = 1;
+            Move();
+        }
+
+        if (Input.GetKey("down"))
+        {
+            motionDirectionY = -1;
+            Move();
+        }
+
+    }
+
+    private void Rotate()
+    {
+        float rotationVelocityDegree = rotationSpeedDegree * rotationDirection;
+        rotationAngleDegree += rotationVelocityDegree * Time.deltaTime;
+
+        //make sure that rotationAngleDegree within 0-360
+        rotationAngleDegree = (rotationAngleDegree + 360) % 360;
+        transform.Rotate(Vector3.up, rotationVelocityDegree * Time.deltaTime); //Vector.up is Y-Axis
+    }
+
+    private void Move()
+    {
+        //Add the following
+        float motionY = motionDirectionY;
+
+        //convert degree to radian
+        double rotationAngleRadian = ((float)rotationAngleDegree / 360.0) * (Math.PI * 2.0);
+        float motionX = (float)Math.Sin(rotationAngleRadian) * motionDirectionXZ;
+        float motionZ = (float)Math.Cos(rotationAngleRadian) * motionDirectionXZ;
+
+        motionVelocity = new Vector3(motionX, motionY, motionZ);
+
+        //nomoralized vector to represent the directions of motionVelocity
+        motionVelocity.Normalize();
+
+        motionVelocity = new Vector3(motionVelocity.x * motionSpeedXZ, motionVelocity.y * motionSpeedY, motionVelocity.z * motionSpeedXZ);
+        transform.position += motionVelocity * Time.deltaTime;
+
+        rotationDirection = 0;
+        motionDirectionXZ = 0;
+
+        
+
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        //you will update these in a later step
+        if (c.gameObject.tag == "Obstacle")
+        {
+            Debug.Log("Game Over: hit obstacle");
+        }
+        else if (c.gameObject.tag == "Ground")
+        {
+            Debug.Log("Game Over: hit ground");
+        }
+        if (c.gameObject.tag == "Gap")
+        {
+            Debug.Log("Passed through a gap!");
+        }
+    }
+
+    void OnTriggerExit(Collider c)
+    {
+        if (c.gameObject.tag == "Gap")
+        {
+            Debug.Log("Exited a gap!");
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameController : MonoBehaviour
+{
+    public GameObject obstacle;
+
+    private ArrayList obstacles;
+
+    public Text scoreText;
+    public Text gameOverText;
+
+    // Use this for initialization
+    void Start()
+    {
+        obstacles = new ArrayList();
+        CreateObstacles();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Obstacles remaining: " + obstacles.Count;
+
+        //to be completed
+    }
+
+    public void CreateObstacles()
+    {
+        //you should create at least 3 obstacles with different rotations 
+        //and in different positions
+        //note: we are adding them to the ArrayList created above
+
+        //Instantiate can take the transform (position vector) 
+        //and rotation quaternion as parameters
+
+        obstacles.Add(
+            Instantiate(obstacle, new Vector3(4, 4, 4), Quaternion.identity)
+        );
+
+        //Quaternion.AngleAxis creates a Quaternion object that is defined by 
+        //the number of degrees of rotation around a provided axis. 
+        //Below we provide the up axis (or y-axis)
+        obstacles.Add(
+            Instantiate(obstacle, new Vector3(-10, 8, 6), Quaternion.AngleAxis(45f, Vector3.up))
+        );
+    }
+
+    //This method is to be called by the FlyingCraft when it successfully
+    //makes it through a gap. The first parameter is the obstacle,
+    //the second parameter is the FlyingCraft game object.
+    public void Score(GameObject obstacle, GameObject sender)
+    {
+        //check if the obstacle is in the list
+        //i.e., has not yet been passed through
+        if (obstacles.Contains(obstacle))
+        {
+            obstacles.Remove(obstacle);
+        }
+
+        if (obstacles.Count == 0)
+        {
+            Win();
+            sender.SendMessage("Stop");
+        }
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "Obstacle")
+        {
+            gameOn = false;
+            gameController.GameOver();
+        }
+        else if (c.gameObject.tag == "Ground")
+        {
+            gameOn = false;
+            gameController.GameOver();
+        }
+        if (c.gameObject.tag == "Gap")
+        {
+            inGap = true;
+        }
+    }
+
+    void OnTriggerExit(Collider c)
+    {
+        if (inGap)
+        {
+            inGap = false;
+
+            //note the line below is necessary to access the whole Prefab
+            //obstacle object. The collider that is return only refer to the 
+            //the gap cube
+            GameObject parentObject = c.gameObject.transform.parent.gameObject;
+
+            //call the score method with the correct obstacle and
+            //a reference to this FlyingCraft object
+            gameController.Score(parentObject, gameObject);
+        }
+    }
+}
